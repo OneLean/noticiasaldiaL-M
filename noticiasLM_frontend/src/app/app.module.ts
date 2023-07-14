f11d94bf7a42e42dcb1ec276fdcd3f67c3267def
OneLean <leandro.montellano@gmail.com>
2023-07-14 16:03:50 -0300

    Pequeña maquetacion de el creador de noticias

--- noticiasLM_frontend/src/app/app.module.ts
+++ noticiasLM_frontend/src/app/app.module.ts
@@ -3,7 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+// import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { EditorModule } from '@tinymce/tinymce-angular';
+
+
+// Nuestras views
 import { CreateViewSectionComponent } from './components/create-view-section/create-view-section.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +20,11 @@ import { CreateViewSectionComponent } from './components/create-view-section/cre
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    // CKEditorModule,
+    EditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- noticiasLM_frontend/src/app/components/create-view-section/create-view-section.component.ts
+++ noticiasLM_frontend/src/app/components/create-view-section/create-view-section.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+// import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
+import * as Editor from 'ckeditor5-custom-build/build/ckeditor';
+// import * as Editor from '../../../../ckCustomBuild/build/ckeditor'
+
+import { Font } from '@ckeditor/ckeditor5-font';
+
 
 @Component({
   selector: 'app-create-view-section',
@@ -6,5 +12,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-view-section.component.css']
 })
 export class CreateViewSectionComponent {
-  
+  public Editor = Editor;
+  public config = {
+    toolbar: {
+      items: [
+        'undo',
+        'redo',
+        '|',
+        'bold',
+        'italic',
+        'underline',
+        'fontFamily',
+        '|',
+        'heading',
+        'alignment',
+        'bulletedList',
+        'numberedList',
+        '|',
+        'outdent',
+        'indent',
+        '|',
+        'imageInsert',
+        'link',
+        'blockQuote',
+        'mediaEmbed',
+        'insertTable',
+        'linkImage', 
+        '-',
+        'fontSize',
+        'fontColor',
+        'fontBackgroundColor',
+        'strikethrough',
+        'highlight',
+        'removeFormat',
+        '|',
+        'superscript',
+        'subscript',
+        'findAndReplace',
+        'specialCharacters',
+        'horizontalLine',
+        
+      ],
+      shouldNotGroupWhenFull: true
+    },
+    language: 'es',
+    image: {
+      toolbar: [
+        'imageTextAlternative',
+        'toggleImageCaption',
+        'imageStyle:inline',
+        'imageStyle:block',
+        'imageStyle:side',
+        'linkImage'
+      ]
+    },
+    table: {
+      contentToolbar: [
+        'tableColumn',
+        'tableRow',
+        'mergeTableCells',
+        'tableCellProperties',
+        'tableProperties'
+      ]
+    }
+  }
 }
